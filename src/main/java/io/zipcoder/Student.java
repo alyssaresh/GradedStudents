7404fc98b6ba1c5fcad64349e47541368577dec0
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-11-07 16:53:45 -0500

    finished Student class and tests for that class

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,35 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double>examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public int getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,51 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+
 public class StudentTest {
 
+    @Test
+    public void testSetFirstName() {
+        ArrayList<Double> scores = new ArrayList<>();
+        Student student = new Student("Deepti", "", scores);
+        String expected = "Deepti";
+
+        // When
+        student.setFirstName(expected);
+        String actual = student.getFirstName();
+
+        // Then (also tests getFirstName)
+        Assert.assertEquals(expected, actual);
+        System.out.print(student.getFirstName());
+    }
+
+    @Test
+    public void testSetLastName() {
+        ArrayList<Double> scores = new ArrayList<>();
+        Student student = new Student("Deepti", "Mishra", scores);
+        String expected = "Mishra";
+
+        // When
+        student.setLastName(expected);
+        String actual = student.getLastName();
+
+        // Then (also tests getLastName)
+        Assert.assertEquals(expected, actual);
+        System.out.print(student.getLastName());
+    }
+
+    @Test
+    public void testGetNumberOfExamsTaken() {
+        int expected = 0;
+        //when
+        ArrayList<Double> scores = new ArrayList<>();
+        Student student = new Student("Deepti", "Mishra", scores);
+        int actual = student.getNumberOfExamsTaken();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
